09279f5af07ca021dd60816c31b63ae26b3b9e9d
Jono453 <jonathan.loong938@gmail.com>
2022-09-26 22:03:39 +1000

    add gmapping_slam launch file

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -6,7 +6,9 @@ xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 xterm -e "roslaunch my_robot amcl.launch" &
 sleep 5
-xterm -e "rosrun teleop_twist_keyboard teleop_twist_keyboard.py" 
+xterm -e "rosrun teleop_twist_keyboard teleop_twist_keyboard.py" &
+sleep 5
+xterm -e "roslaunch my_robot my_robot_slam_gmapping.launch"
 
 
 
